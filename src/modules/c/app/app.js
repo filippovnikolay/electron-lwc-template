10f079d0ca616e307862f1f9c0e56cae884edd5e
Nikolay Filippov <nikolay_filippov@epam.com>
2025-10-14 17:02:10 +0300

    * fixed SLDS import

--- src/modules/c/app/app.js
+++ src/modules/c/app/app.js
@@ -1,28 +1,5 @@
 import { LightningElement } from 'lwc';
 
 export default class App extends LightningElement {
-    handleClick() {
-        alert('Login clicked');
-    }
-
-    _selected = [];
-
-    get options() {
-        return [
-            { label: 'English', value: 'en' },
-            { label: 'German', value: 'de' },
-            { label: 'Spanish', value: 'es' },
-            { label: 'French', value: 'fr' },
-            { label: 'Italian', value: 'it' },
-            { label: 'Japanese', value: 'ja' },
-        ];
-    }
-
-    get selected() {
-        return this._selected.length ? this._selected : 'none';
-    }
-
-    handleChange(e) {
-        this._selected = e.detail.value;
-    }
+    static renderMode = 'light';
 }
